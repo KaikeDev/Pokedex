3b7085a0ad8f6cd5098315240d637aaf2795373d
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2024-09-15 19:08:03 -0300

    Adicionando os detalhes dos Pokemons

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,13 +1,48 @@
+import { PokeHeaderComponent } from './../../shared/poke-header/poke-header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { PokeApiService } from '../../services/poke-api.service';
+import { forkJoin } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, PokeHeaderComponent, RouterLink, CommonModule],
   templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+  styleUrl: './details.component.scss',
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon';
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
 
+  public pokemon:any
+  public isLoading: boolean = false;
+  public apiError: boolean = false;
+
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private pokeApiService: PokeApiService
+  ) {}
+  ngOnInit(): void {
+    this.getPokemon;
+  }
+  get getPokemon() {
+    const id = this.activeRoute.snapshot.params['id'];
+    const pokemon = this.pokeApiService.apiGetPokens(
+      `${this.urlPokemon}/${id}`
+    );
+    const name = this.pokeApiService.apiGetPokens(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        this.pokemon = res;
+        this.isLoading = true;
+      },
+      error => {
+        this.apiError = true;
+      }
+    );
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { PokeHeaderComponent } from '../../shared/poke-header/poke-header.compon
 import { PokeSearchComponent } from '../../shared/poke-search/poke-search.component';
 import { PokeListComponent } from '../../shared/poke-list/poke-list.component';
 import { HttpClientModule } from '@angular/common/http';
+import { DetailsComponent } from "../details/details.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent,HttpClientModule],
+  imports: [PokeHeaderComponent, PokeSearchComponent, PokeListComponent, HttpClientModule, DetailsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
